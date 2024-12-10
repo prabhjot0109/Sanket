1bb3f1a7f74dc122b222c8e4772d58777a7be7fa
Prabhjot Singh Assi <prabhjotassi16@gmail.com>
2024-12-10 19:59:49 +0530

    App issues fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-// @dart=2.9
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:sanket/speechToSign.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -33,5 +31,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
